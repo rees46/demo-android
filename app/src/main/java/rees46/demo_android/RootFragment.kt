7f08aeaaae2ee5b70a60bd90522660a3fff04ac4
Maria Anchugova <xeewisom@gmail.com>
2024-07-08 18:10:55 +0500

    refactor: renamed StartScreenFragment to RootFragment

--- app/src/main/java/rees46/demo_android/RootFragment.kt
+++ app/src/main/java/rees46/demo_android/RootFragment.kt
@@ -4,16 +4,16 @@ import android.os.Handler
 import android.os.Looper
 import androidx.navigation.fragment.findNavController
 import rees46.demo_android.core.view.BaseFragment
-import rees46.demo_android.databinding.FragmentStartScreenBinding
+import rees46.demo_android.databinding.FragmentRootBinding
 
-class StartScreenFragment
-    : BaseFragment<FragmentStartScreenBinding>(FragmentStartScreenBinding::inflate) {
+class RootFragment
+    : BaseFragment<FragmentRootBinding>(FragmentRootBinding::inflate) {
 
     override fun onResume() {
         super.onResume()
 
         Handler(Looper.getMainLooper()).postDelayed({
-            findNavController().navigate(R.id.action_startScreenFragment_to_mainFragment)
+            findNavController().navigate(R.id.action_rootFragment_to_mainFragment)
         }, 1000)
     }
 }
