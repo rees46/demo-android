ceaeee3bac54fd35f2ee7681a5a8ae7ae0fa08bc
Maria Anchugova <xeewisom@gmail.com>
2024-07-08 06:26:20 +0500

    refactor: removed base card product view

--- app/src/main/java/rees46/demo_android/feature/cardProduct/CardProductView.kt
+++ app/src/main/java/rees46/demo_android/feature/cardProduct/CardProductView.kt
@@ -3,18 +3,27 @@ package rees46.demo_android.feature.cardProduct
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatRatingBar
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.button.MaterialButton
 import rees46.demo_android.R
-import rees46.demo_android.base.BaseCardProductView
+import rees46.demo_android.core.utils.ImageUtils
 import rees46.demo_android.entities.products.ProductEntity
 import rees46.demo_android.feature.card_product.CardAction
 
 class CardProductView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
-) : BaseCardProductView(context, attrs, R.layout.view_card_product) {
+) : ConstraintLayout(context, attrs) {
 
+    private lateinit var productImageView: ImageView
+    internal lateinit var producerNameTextView: TextView
+    private lateinit var productNameTextView: TextView
+    private lateinit var ratingBar: AppCompatRatingBar
+    private lateinit var oldPriceTextView: TextView
+    private lateinit var priceTextView: TextView
     private lateinit var descriptionTextView: TextView
     private lateinit var reviewsTextView: TextView
     private lateinit var saleTextView: TextView
@@ -24,10 +33,18 @@ class CardProductView @JvmOverloads constructor(
     private lateinit var addToCartButton: MaterialButton
 
     init {
+        inflate(context, R.layout.view_card_product, this)
+
         initViews()
     }
 
     private fun initViews() {
+        productImageView = findViewById(R.id.product_image)
+        producerNameTextView = findViewById(R.id.producer_name_text)
+        productNameTextView = findViewById(R.id.product_name_text)
+        ratingBar = findViewById(R.id.product_rating_bar)
+        oldPriceTextView = findViewById(R.id.old_price_text)
+        priceTextView = findViewById(R.id.price_text)
         descriptionTextView = findViewById(R.id.description_text)
         reviewsTextView = findViewById(R.id.reviews_text)
         saleTextView = findViewById(R.id.sale_text)
@@ -43,8 +60,12 @@ class CardProductView @JvmOverloads constructor(
         plusButton.setOnClickListener { onCartActionClick.invoke(CardAction.INCREASE) }
     }
 
-    override fun updateProduct(product: ProductEntity) {
-        super.updateProduct(product)
+    fun updateProduct(product: ProductEntity) {
+        ImageUtils.updateImage(this, productImageView, product.pictureUrl)
+
+        productNameTextView.text = product.name
+        producerNameTextView.text = product.producerName
+        priceTextView.text = product.priceFormatted
         descriptionTextView.text = product.description
     }
 
