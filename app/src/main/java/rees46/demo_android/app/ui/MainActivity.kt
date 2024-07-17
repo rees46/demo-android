ce94e26c56b4739fb68b3b78ac0aa64da9e68477
Maria Anchugova <xeewisom@gmail.com>
2024-07-17 15:56:33 +0500

    feat (status bar): add status bar

--- app/src/main/java/rees46/demo_android/app/ui/MainActivity.kt
+++ app/src/main/java/rees46/demo_android/app/ui/MainActivity.kt
@@ -1,7 +1,6 @@
 package rees46.demo_android.app.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import rees46.demo_android.R
@@ -9,7 +8,6 @@ import rees46.demo_android.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
-        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
