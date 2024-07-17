6f8f6ff39dd41f5b91c7f8d2fd785eca67ba68c6
Maria Anchugova <xeewisom@gmail.com>
2024-07-17 11:01:16 +0500

    feat (ui): add shop button to card product

--- app/src/main/java/rees46/demo_android/app/ui/products/adapter/CardProductView.kt
+++ app/src/main/java/rees46/demo_android/app/ui/products/adapter/CardProductView.kt
@@ -3,6 +3,7 @@ package rees46.demo_android.app.ui.products.adapter
 import android.content.Context
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatRatingBar
@@ -17,11 +18,12 @@ class CardProductView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs) {
 
     private lateinit var productImageView: ImageView
-    internal lateinit var producerNameTextView: TextView
+    private lateinit var producerNameTextView: TextView
     private lateinit var productNameTextView: TextView
     private lateinit var ratingBar: AppCompatRatingBar
     private lateinit var oldPriceTextView: TextView
     private lateinit var priceTextView: TextView
+    private lateinit var shopButton: Button
 
     init {
         inflate(context, R.layout.view_products_card_product, this)
@@ -37,10 +39,13 @@ class CardProductView @JvmOverloads constructor(
         ratingBar = findViewById(R.id.product_rating_bar)
         oldPriceTextView = findViewById(R.id.old_price_text)
         priceTextView = findViewById(R.id.price_text)
+        shopButton = findViewById(R.id.shop_button)
     }
 
     private fun setupView() {
         oldPriceTextView.paintFlags += Paint.STRIKE_THRU_TEXT_FLAG
+
+        shopButton.setOnClickListener { }
     }
 
     internal fun updateProduct(product: ProductDto) {
