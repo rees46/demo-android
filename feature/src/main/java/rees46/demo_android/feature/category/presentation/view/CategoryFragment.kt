e902adc71001d5f36e0a89d8e7af01e2a1ab6c37
Maria Anchugova <xeewisom@gmail.com>
2024-07-18 22:09:08 +0500

    fix (category): inflate category fragment

--- feature/src/main/java/rees46/demo_android/feature/category/presentation/view/CategoryFragment.kt
+++ feature/src/main/java/rees46/demo_android/feature/category/presentation/view/CategoryFragment.kt
@@ -1,10 +1,22 @@
 package rees46.demo_android.feature.category.presentation.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import rees46.demo_android.feature.category.presentation.viewmodel.CategoryViewModel
+import rees46.demo_android.databinding.FragmentCategoryBinding
 
 class CategoryFragment : Fragment() {
 
-    private val viewModel: CategoryViewModel by viewModels()
+    private lateinit var binding: FragmentCategoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
