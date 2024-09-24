06adb014b144ccbc9c1bb8c3d1a45dc9cb5c7cec
Daniel Green <gdl@rees46.ru>
2024-09-24 10:01:18 +0200

    feat: Updated sdk version and added localizable strings

--- feature/src/main/java/rees46/demo_android/feature/home/presentation/view/NewFeaturesFragment.kt
+++ feature/src/main/java/rees46/demo_android/feature/home/presentation/view/NewFeaturesFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.personalization.SDK
 import org.koin.android.ext.android.inject
+import rees46.demo_android.R
 import rees46.demo_android.databinding.FragmentNewFeaturesBinding
 
 class NewFeaturesFragment : Fragment() {
@@ -31,72 +32,65 @@ class NewFeaturesFragment : Fragment() {
 
     private fun handleInAppNotifications() {
         //TODO remove
-        val debugTitle = "Привет,мы на связи"
-        val debugMessage =
-            "И мы к вам с хорошими новостями. Совсем скоро мы проведем вебинар по поиску на сайте — там будет масса полезной информации, которая поможет бустануть конверсию и повысить лояльность аудитории. Приходите!"
-        val debugImageUrl =
-            "https://ih1.redbubble.net/image.2487413451.3407/fmp,x_small,gloss,study,product,750x1000.jpg"
-        val buttonNegative = "Cancel"
-        val buttonPositive = "OK"
         with(binding) {
 
-//            alertDialogButton.setOnClickListener {
-//                sdk.inAppNotificationManager.showAlertDialog(
-//                    fragmentManager = childFragmentManager,
-//                    title = debugTitle,
-//                    message = debugMessage,
-//                    buttonText = buttonPositive
-//                )
-//            }
-//
-//            fullScreenDialogButton.setOnClickListener {
-//                sdk.inAppNotificationManager.showFullScreenDialog(
-//                    fragmentManager = childFragmentManager,
-//                    title = debugTitle,
-//                    message = debugMessage,
-//                    imageUrl = debugImageUrl,
-//                    buttonNegativeText = buttonNegative,
-//                    buttonPositiveText = buttonPositive,
-//                    onNegativeClick = {
-//                        Log.d(requireContext().javaClass.name, ": onNegativeClick")
-//                    },
-//                    onPositiveClick = {
-//                        Log.d(requireContext().javaClass.name, ": onPositiveClick")
-//                    },
-//                )
-//            }
-//
-//            bottomSheetDialogButton.setOnClickListener {
-//                sdk.inAppNotificationManager.showBottomSheetDialog(
-//                    fragmentManager = childFragmentManager,
-//                    title = debugTitle,
-//                    message = debugMessage,
-//                    imageUrl = debugImageUrl,
-//                    buttonNegativeText = buttonNegative,
-//                    buttonPositiveText = buttonPositive,
-//                    onNegativeClick = {
-//                        Log.d(requireContext().javaClass.name, ": onNegativeClick")
-//                    },
-//                    onPositiveClick = {
-//                        Log.d(requireContext().javaClass.name, ": onPositiveClick")
-//                    },
-//                )
-//            }
-//
-//            snackBarButton.setOnClickListener {
-//                sdk.inAppNotificationManager.showSnackBar(
-//                    view = findViewById(android.R.id.content),
-//                    message = debugTitle,
-//                    buttonNegativeText = buttonNegative,
-//                    buttonPositiveText = buttonPositive,
-//                    onNegativeClick = {
-//                        Log.d(requireContext().javaClass.name, ": onNegativeClick")
-//                    },
-//                    onPositiveClick = {
-//                        Log.d(requireContext().javaClass.name, ": onPositiveClick")
-//                    },
-//                )
-//            }
+            alertDialogButton.setOnClickListener {
+                sdk.inAppNotificationManager.showAlertDialog(
+                    fragmentManager = childFragmentManager,
+                    title = requireContext().getString(R.string.alert_dialog_title),
+                    message = requireContext().getString(R.string.alert_dialog_message),
+                    buttonText = requireContext().getString(R.string.alert_dialog_button_accept)
+                )
+            }
+
+            fullScreenDialogButton.setOnClickListener {
+                sdk.inAppNotificationManager.showFullScreenDialog(
+                    fragmentManager = childFragmentManager,
+                    title = requireContext().getString(R.string.alert_dialog_title),
+                    message = requireContext().getString(R.string.alert_dialog_message),
+                    imageUrl = "https://ih1.redbubble.net/image.2487413451.3407/fmp,x_small,gloss,study,product,750x1000.jpg",
+                    buttonNegativeText = requireContext().getString(R.string.alert_dialog_button_decline),
+                    buttonPositiveText = requireContext().getString(R.string.alert_dialog_button_accept),
+                    onNegativeClick = {
+                        Log.d(requireContext().javaClass.name, ": onNegativeClick")
+                    },
+                    onPositiveClick = {
+                        Log.d(requireContext().javaClass.name, ": onPositiveClick")
+                    },
+                )
+            }
+
+            bottomSheetDialogButton.setOnClickListener {
+                sdk.inAppNotificationManager.showBottomSheetDialog(
+                    fragmentManager = childFragmentManager,
+                    title = requireContext().getString(R.string.alert_dialog_title),
+                    message = requireContext().getString(R.string.alert_dialog_message),
+                    imageUrl = "https://ih1.redbubble.net/image.2487413451.3407/fmp,x_small,gloss,study,product,750x1000.jpg",
+                    buttonNegativeText = requireContext().getString(R.string.alert_dialog_button_decline),
+                    buttonPositiveText = requireContext().getString(R.string.alert_dialog_button_accept),
+                    onNegativeClick = {
+                        Log.d(requireContext().javaClass.name, ": onNegativeClick")
+                    },
+                    onPositiveClick = {
+                        Log.d(requireContext().javaClass.name, ": onPositiveClick")
+                    },
+                )
+            }
+
+            snackBarButton.setOnClickListener {
+                sdk.inAppNotificationManager.showSnackBar(
+                    view = requireView(),
+                    message = requireContext().getString(R.string.alert_dialog_title),
+                    buttonNegativeText = requireContext().getString(R.string.alert_dialog_button_decline),
+                    buttonPositiveText = requireContext().getString(R.string.alert_dialog_button_accept),
+                    onNegativeClick = {
+                        Log.d(requireContext().javaClass.name, ": onNegativeClick")
+                    },
+                    onPositiveClick = {
+                        Log.d(requireContext().javaClass.name, ": onPositiveClick")
+                    },
+                )
+            }
         }
     }
 }
