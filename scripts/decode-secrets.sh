8a88b76f184999dde0ca53872de8eac86eea22ba
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-03-10 16:05:31 +0300

    refactor(github): useless

--- scripts/decode-secrets.sh
+++ scripts/decode-secrets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "${PLAY_ACCOUNT_AS_BASE64}" | base64 --decode > app/play-account.json
-echo "${KEYSTORE_AS_BASE64}" | base64 --decode > app/keystore.jks
-echo "${GOOGLE_SERVICES_FILE_AS_BASE64}" | base64 --decode > app/google-services.json
+echo "${PLAY_ACCOUNT_AS_BASE64}" | base64 --decode > ../app/play-account.json
+echo "${KEYSTORE_AS_BASE64}" | base64 --decode > ../app/keystore.jks
+echo "${GOOGLE_SERVICES_FILE_AS_BASE64}" | base64 --decode > ../app/google-services.json
