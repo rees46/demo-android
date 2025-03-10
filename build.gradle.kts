c67bcea93b2c3724bfed284583c5b5c4640952c5
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-03-10 16:22:29 +0300

    refactor(github): useless

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
+	id("com.android.application") version "8.5.2"
 }
 
 buildscript {
