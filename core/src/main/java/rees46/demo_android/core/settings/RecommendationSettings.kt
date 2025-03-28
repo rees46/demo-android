546dd1944df98122e02051075c6097147a76c1a4
Arina Gazhina <gazhina@internet.ru>
2025-03-28 14:32:48 +0300

    feat(common): change recommendation block code

--- core/src/main/java/rees46/demo_android/core/settings/RecommendationSettings.kt
+++ core/src/main/java/rees46/demo_android/core/settings/RecommendationSettings.kt
@@ -1,7 +1,7 @@
 package rees46.demo_android.core.settings
 
 object RecommendationSettings {
-    const val SIMPLE_RECOMMENDED_CODE = "a043dbc2f852ffe18861a2cdfc364ef2"
+    const val SIMPLE_RECOMMENDED_CODE = "5a57812604806d58d3d98f8e9480cb97"
     const val CART_RECOMMENDED_CODE = "2dbebc39bee259b118bcc0ac3fa74a42"
     const val ALSO_LIKE_RECOMMENDED_CODE = "a043dbc2f852ffe18861a2cdfc364ef2"
 }
